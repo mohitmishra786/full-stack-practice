c377cc78c32ecbf2480002be384dcbe69ce0526c
mohitmishra786 <immadmohit@gmail.com>
2023-06-30 15:31:55 +0530

    Created a socket server and client

--- index-ws.js
+++ index-ws.js
@@ -15,5 +15,26 @@ server.listen(3000, function(){
 // Now here firstly we will do `npm i ws`
 
 const WebSocketServer = require('ws').Server;
-
 const wss = new WebSocketServer({server: server});
+
+wss.on('connection', function connection(ws){
+    const numClients = wss.clients.size;
+    console.log('Clients connected', numClients);
+
+    wss.broadcast(`Current Visitors: ${numClients}`);
+
+    if(ws.readyState === ws.OPEN){
+        ws.send('Welcome to my server');
+    }
+
+    ws.on('close', function close(){
+        wss.broadcast(`Current Vistors: ${numClients}`);
+        console.log('A client has disconnected');
+    });
+})
+
+wss.broadcast = function broadcast(data){
+    wss.clients.forEach(function each(client){
+        client.send(data);
+    });
+}
